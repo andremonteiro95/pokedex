673549d262d61c58d4e0dadfa25a9b92ed259702
andremonteiro95 <andremonteiro95@gmail.com>
2022-11-27 22:36:52 +0000

    Add navbar links

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,17 @@
 import React, { PropsWithChildren } from 'react'
-import { Link } from 'react-router-dom'
+import { LinkProps, NavLink } from 'react-router-dom'
+
+const NavbarLink = (props: LinkProps) => {
+  return <NavLink {...props} className="text-white font-semibold" />
+}
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className='min-h-screen w-full bg-slate-100'>
       <div className='h-16 w-full bg-red-500 border-b-4 border-black'>
-        <div className='container mx-auto'>
-          <Link to="/favorites">Favorites</Link>
+        <div className='container mx-auto h-full px-8 gap-8 flex items-center'>
+          <NavbarLink to="/">Home</NavbarLink>
+          <NavbarLink to="/favorites">Favorites</NavbarLink>
         </div>
       </div>
       <div className='container mx-auto p-8'>
